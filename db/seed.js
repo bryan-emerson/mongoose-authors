c67764db605ef4b3a55e365091fbc1375c16124b
Zakk Fleischmann <fleischmann@teamsubjectmatter.com>
2019-11-05 21:49:34 -0500

    add prompts to index.js

---
+++ db/seed.js
@@ -0,0 +1,9 @@
+const Author = require("../models/Author");
+const data = require("./authors.json");
+
+Author.deleteMany({}).then(() => {
+  Author.create(data).then(authors => {
+    console.log(authors);
+    process.exit()
+  });
+});
